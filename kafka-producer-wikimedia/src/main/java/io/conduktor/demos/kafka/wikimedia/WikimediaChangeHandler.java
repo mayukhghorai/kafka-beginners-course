226829b8e59e18b27800b6cb6bd49732848f1319
mayukh <mayukh.ghorai@yahoo.com>
2023-09-01 23:02:56 +0530

    First commit

---
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,46 @@
+package io.conduktor.demos.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.types.Field;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangeHandler implements EventHandler {
+    private static final Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class.getSimpleName());
+    KafkaProducer<String, String> producer;
+    String topic;
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> producer, String topic) {
+        this.producer = producer;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen()  {
+    //nothing here
+    }
+
+    @Override
+    public void onClosed() {
+        producer.close();
+    }
+
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent) {
+        log.info(messageEvent.getData());
+        producer.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String comment) {
+
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        log.error("Error in event Record", t);
+    }
+}
